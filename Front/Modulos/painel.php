1c79e467e66bbb3bdddfba1e4361d70493ec4cd5
Udanidan <d4nizoca@gmail.com>
2025-11-14 09:05:10 -0300

    Atualização de mokup

--- Front/Modulos/painel.php
+++ Front/Modulos/painel.php
@@ -3,8 +3,8 @@
 <section id="painel">
     <img src="https://bkpsitecpsnew.blob.core.windows.net/uploadsitecps/sites/197/2024/12/centro-paula-souza.jpg" alt="">
     <article>
-        <h1><?php if(isset($_SESSION['usuario'])){echo $_SESSION['usuario'];} ?></h1>
-        <p>Professores: <?php  ?></p>
-        <p>salas: <?php  ?></p>
+        <h1>Olá</h1>
+        <p>Professores: </p>
+        <p>salas: </p>
     </article>
 </section>
