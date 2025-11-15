928282afe771b31cb69d5eb7366865aa4987b28d
v1n1k4 <vini.castilho09@gmail.com>
2025-11-15 15:08:08 -0300

    Update footer.php

--- Front/Modulos/footer.php
+++ Front/Modulos/footer.php
@@ -1,5 +1,25 @@
-    <footer>
-        <p>Todos os direitos reservados</p>
-    </footer>
+<footer>
+    <div class="footer-content">
+        <div class="footer-brand">
+            CLAEX
+        </div>
+        <div class="footer-links">
+            <div class="footer-col">
+                <h4>Sobre</h4>
+                <a href="#">Quem Somos</a>
+                <a href="#">Contato</a>
+            </div>
+            <div class="footer-col">
+                <h4>Legal</h4>
+                <a href="#">Termos de Uso</a>
+                <a href="#">Política de Privacidade</a>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        &copy; 2025 CLAEX. Todos os direitos reservados.
+    </div>
+    <link rel="stylesheet" href="css/style.css">
+</footer>
 </body>
 </html>
