8d3b604b014699136e203fcf6d7433ffaa2bff7e
udanidanudanidan <d4nizoca@gmail.com>
2026-05-30 13:24:56 -0300

    grande update em genetico.py
    Autalizações:
    
    1. Adicionada função validarVagas() onde verifica a posição onde aulas vazias se encontram evitando que as mesmas ocorram durante o dia ficando posicionadas preferencialmente no começo ou fim do dia letivo.
    
    2. Foi adicionada a verificação de conflitos de aulas em mais de uma sala, onde agora ao invés de ser fornecida uma única grade para verificar conflitos é passado uma lista com todas as grades existentes no momento, verificando se não há conflitos com nenhuma delas (foram geradas ao todo 6 grades diferentes sem conflitos entre si com qualidade boa a decente embora seja perceptível uma queda após a quinta geração devido a diminuição de possibilidades totais, houve também desempenho satisfatório onde uma maquina com processador single core e 4 GB de memoria RAM DDR3 conseguiram processar 40 gerações de 40 indivíduos com latência menor que 2 segundos).
    
    3. Por fim, foram realizados testes de desempenho, melhorias no código e correções menores além dá remoção de partes não utilizadas e melhoria em comentarios.

--- Back/api/genetico.py
+++ Back/api/genetico.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------------------------------------------
+# Projeto: CLAEX
+# Objetivo: Realizar geração de grade de forma automatica, está sendo utilizada a base de um algotimo genetico
+# Ultima atulização: 30/05/2026 Dev: Udanidan
+# Devs que alteraram o codigo até o momento: Udanidan
+# -----------------------------------------------------------------------------------
+
 from random import choice, randint
 import copy
 
@@ -9,20 +16,69 @@ import copy
 #     ['-', '-', 'roblox']  # sexta
 # ]
 
+# antes do algotimo aprimorar
+# gradeSala2 = [
+#     ["mat", "port", "hist", "geo", "cien", "ing", "edfis", "art"],   # segunda
+#     ["mat", "port", "quim", "fis", "bio", "geo", "ing", "red"],      # terça
+#     ["port", "mat", "hist", "cien", "art", "edfis", "geo", "ing"],   # quarta
+#     ["mat", "port", "fis", "quim", "bio", "hist", "red", "edfis"],   # quinta
+#     ["port", "mat", "geo", "hist", "cien", "art", "ing", "proj"]     # sexta
+# ]
 gradeSala2 = [
-    ["mat", "port", "hist", "geo", "cien", "ing", "edfis", "art"],   # segunda
-    ["mat", "port", "quim", "fis", "bio", "geo", "ing", "red"],      # terça
-    ["port", "mat", "hist", "cien", "art", "edfis", "geo", "ing"],   # quarta
-    ["mat", "port", "fis", "quim", "bio", "hist", "red", "edfis"],   # quinta
-    ["port", "mat", "geo", "hist", "cien", "art", "ing", "proj"]     # sexta
+    ['port', 'port', 'ing', 'ing', 'mat', 'mat', 'hist', 'hist'],
+    ['--', 'art', 'quim', 'quim', 'art', 'fis', 'ing', 'ing'],
+    ['--', '--', 'edfis', 'edfis', 'cien', 'cien', 'geo', 'geo'],
+    ['port', 'port', 'fis', 'art', 'hist', 'hist', 'bio', 'bio'],
+    ['--', 'proj', 'geo', 'geo', 'red', 'red', 'mat', 'mat']
 ]
+
+gradeSala3 = [
+    ['hist', 'hist', 'red', 'red', 'bio', 'bio', 'port', 'port'],
+    ['--', 'geo', 'ing', 'ing', 'hist', 'hist', 'art', 'art'],
+    ['--', 'proj', 'mat', 'mat', 'geo', 'geo', 'port', 'port'],
+    ['--', 'geo', 'cien', 'cien', 'mat', 'mat', 'ing', 'ing'],
+    ['--', 'art', 'edfis', 'edfis', 'fis', 'fis', 'quim', 'quim']
+]
+
+gradeSala4 = [
+    ['--', '--', 'edfis', 'edfis', 'geo', 'geo', 'cien', 'cien'],
+    ['bio', 'bio', 'port', 'port', 'mat', 'mat', '--', '--'],
+    ['red', 'red', 'geo', 'geo', 'mat', 'mat', 'hist', 'hist'],
+    ['ing', 'ing', 'art', 'proj', 'quim', 'quim', 'port', 'port'],
+    ['fis', 'fis', 'art', 'art', 'ing', 'ing', 'hist', 'hist']
+]
+
+gradeSala5 = [
+    ['--', '--', 'quim', 'quim', 'proj', 'cien', 'ing', 'ing'],
+    ['--', 'hist', 'art', 'geo', 'edfis', 'edfis', 'mat', 'fis'],
+    ['ing', 'ing', 'red', 'red', 'hist', 'hist', 'mat', 'mat'],
+    ['fis', 'mat', 'geo', 'hist', 'port', 'port', 'art', 'art'],
+    ['--', 'cien', 'port', 'port', 'geo', 'geo', 'bio', 'bio']
+]
+
+gradeSala6 = [
+    ['--', 'ing', 'geo', 'art', 'hist', 'hist', 'geo', 'mat'],
+    ['ing', 'ing', 'edfis', 'mat', 'red', 'red', 'port', 'port'],
+    ['--', 'port', 'quim', 'quim', 'bio', 'bio', 'art', 'art'],
+    ['--', 'edfis', 'mat', 'mat', 'geo', 'geo', 'cien', 'cien'],
+    ['--', 'port', 'proj', 'ing', 'hist', 'hist', 'fis', 'fis']
+]
+
+gradeSala7 = [
+    ['--', 'art', 'fis', 'fis', 'quim', 'quim', 'edfis', 'edfis']
+    ['--', 'mat', 'hist', 'hist', 'proj', 'geo', '--', 'geo']
+    ['--', 'mat', 'ing', 'ing', 'port', 'port', 'bio', 'bio']
+    ['art', 'art', 'red', 'red', 'ing', 'ing', 'hist', 'hist']
+    ['geo', 'geo', 'cien', 'cien', 'mat', 'mat', 'port', 'port']
+]
+
 materias = [
+    "--", # "mat",
     "mat",
     "mat",
     "mat",
     "mat",
-    "mat",
-    "port",
+    "--", # "port",
     "port",
     "port",
     "port",
@@ -35,7 +91,7 @@ materias = [
     "geo",
     "geo",
     "geo",
-    "cien",
+    "--", # "cien",
     "cien",
     "cien",
     "fis",
@@ -48,7 +104,7 @@ materias = [
     "ing",
     "ing",
     "ing",
-    "edfis",
+    "--", # "edfis",
     "edfis",
     "edfis",
     "art",
@@ -59,6 +115,7 @@ materias = [
     "proj"
 ]
 
+# --------------- ALGORITIMO DE GERAÇÃO DE GRADE ---------------
 def gerarGrade(materia: list, quantAulas: int):
     materias = materia.copy()
     grade = [["-" for a in range(quantAulas)] for i in range(5)]
@@ -78,25 +135,26 @@ def mostrarGrade(grade: list):
     for e in range(5):
         print(dias[e]+":",grade[e])
 
-# inicio da analise da qualidade da grade
+# --------------- ANALISE DA QUALIDADE DA GRADE ---------------
 
-def validarGrade(grade: list, materias: list):
+def validarGrade(grade: list, gradesExistentes: list, materias: list):
     pontos = 0
     
     pontos += mesmoDia(grade, materias)
     pontos += aulasSeguidas(grade)
+    pontos += validarVagas(grade)
     pontos += limitAulasDiaria(grade, materias)
-    pontos += profIguais(grade) * -10
+    pontos += profIguais(grade, gradesExistentes) * -10
     
     return pontos
 
-def profIguais(grade: list):
+def profIguais(gradeAnalisada: list, gradesExisentes: list):
     iguais = 0 #Ruim
-    for i in range(len(grade)):
-        for a in range(len(grade[i])):
-            if grade[i][a] == gradeSala2[i][a]:
-                # print("posição",i,a,":", grade[i][a], "==", gradeSala2[i][a])
-                iguais += 1
+    for grade in gradesExisentes:
+        for i in range(len(gradeAnalisada)):
+            for a in range(len(gradeAnalisada[i])):
+                if gradeAnalisada[i][a] == grade[i][a] and gradeAnalisada[i][a] != "--":
+                    iguais += 1
     
     return iguais
 
@@ -122,14 +180,25 @@ def aulasSeguidas(grade: list):
                         total -= 2
     return total
 
+def validarVagas(grade: list):
+    total = 0
+    for i in grade:
+        for a in range(len(grade)):
+            if a == 0 and i[a] == '--' or a == len(grade)-1 and i[a] == '':
+                total += 5
+    return total
+
 def limitAulasDiaria(grade: list, materias:list):
     total = 0
-    for i in set(materias):
+    mats = materias.copy()
+    mats.append("--") # garante que aulas vagas não ocorram em excessão no dia, pode ser removido
+    for i in set(mats):
         for a in grade:
             if a.count(i) > 2:
-                total -= a.count(i)-2
+                total -= (a.count(i)-2)*2
     return total
-# treinamentos e alteração da grade
+
+# --------------- TREINAMENTO E ALTERAÇÃO DA GRADE ---------------
 
 def removerConflitos(grade):
     cont = 0
@@ -141,9 +210,7 @@ def removerConflitos(grade):
         cont += 1
         gradem = mutargrade(gradem)
         mats = profIguais(gradem)
-        # print("iguais:", mats[0])
-        # print("diferentes uhuu:", mats[1])
-    
+
     return [gradem, cont]
 
 def mutargrade(grade: list):
@@ -159,20 +226,16 @@ def mutargrade(grade: list):
     
     return grade
 
-def avacarGeracao(grade: list, materia: list, quantGeracoes: int, quantIndividuos: int):
+def avacarGeracao(grade: list, gradesExistentes: list, materia: list, quantGeracoes: int, quantIndividuos: int):
     listaInd = ['' for i in range(quantIndividuos)]
     melhorInd = copy.deepcopy(grade)
-    melhorScore = validarGrade(melhorInd, materia)
-    for i in range(profIguais(melhorInd)):
-        melhorScore -= 10
+    melhorScore = validarGrade(melhorInd, gradesExistentes, materia)
 
     for geracao in range(quantGeracoes):
         for ind in range(quantIndividuos):
             listaInd[ind] = mutargrade(copy.deepcopy(melhorInd))
         for indv in listaInd:
-            indvScore = validarGrade(indv, materia)
-            # for i in range(profIguais(indv)):
-            #     indvScore -= 10
+            indvScore = validarGrade(indv, gradesExistentes, materia)
             # print("pontuação melhor:", melhorScore, "| pontuação verificada:", indvScore, validarGrade(melhorInd, materia) < validarGrade(indv, materia))
             if melhorScore < indvScore:
                 melhorInd = copy.deepcopy(indv)
@@ -180,25 +243,27 @@ def avacarGeracao(grade: list, materia: list, quantGeracoes: int, quantIndividuo
     
     return melhorInd
 
-# testes
+# -------------------- TESTES --------------------
+
 quantAulasDia = 8
-tamanhoPopulacao = 30
-quantGeracoes = 30
+tamanhoPopulacao = 40
+quantGeracoes = 40
 pontuacao = 0
+
+gradesExistentes = [gradeSala2, gradeSala3, gradeSala4, gradeSala5, gradeSala6]
 
 grade1 = gerarGrade(materias, quantAulasDia)
 
-gradeMutada = avacarGeracao(grade1, materias, quantGeracoes, tamanhoPopulacao)
+gradeMutada = avacarGeracao(grade1, gradesExistentes, materias, quantGeracoes, tamanhoPopulacao)
 
 mostrarGrade(grade1)
 print("-------------------")
 mostrarGrade(gradeMutada)
 
-print("grade 2:", validarGrade(gradeSala2, materias))
-print("pontuação da grade inicial:", validarGrade(grade1,materias))
-mats = profIguais(grade1)
+print("pontuação da grade inicial:", validarGrade(grade1, gradesExistentes, materias))
+mats = profIguais(grade1, gradesExistentes)
 print("iguais:", mats)
 
-print("pontuação da grade mutada:", validarGrade(gradeMutada,materias))
-mats = profIguais(gradeMutada)
+print("pontuação da grade mutada:", validarGrade(gradeMutada, gradesExistentes, materias))
+mats = profIguais(gradeMutada, gradesExistentes)
 print("iguais:", mats)
